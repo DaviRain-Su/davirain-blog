8a11d4b4bbb16a4ef248a0b2e19f7eb5dff76319
Davirain <davirain.yin@gmail.com>
2023-02-17 17:47:11 +0800

    add a blog

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,9 +1,9 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
+  title: "davirain's blog",
   description: "An opinionated starter theme for Astro",
   lang: "en-GB",
   ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+  githubUrl: "https://github.com/DaviRain-Su/davirain-blog/tree/main",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
 }
